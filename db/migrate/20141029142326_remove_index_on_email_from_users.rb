2e8e4ff0a27176d744f820688c62a244772e5659
Thomas Taylor-Hall <tjt12@ic.ac.uk>
2014-10-29 14:41:07 +0000

    Initial skeleton rails app.

---
+++ db/migrate/20141029142326_remove_index_on_email_from_users.rb
@@ -0,0 +1,9 @@
+class RemoveIndexOnEmailFromUsers < ActiveRecord::Migration
+  def up
+    remove_index :users, 'email'
+  end
+
+  def down
+    add_index :users, :email, :unique => true
+  end
+end
