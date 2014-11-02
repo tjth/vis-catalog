8ee25828bf45c7bdc0e84e17a51fa30cac62c9bc
Thomas Taylor-Hall <tjt12@ic.ac.uk>
2014-11-02 11:16:09 +0000

    Vis. controller created and some RESTful api functions implemented. Routing for these also done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  resources :visualisations
+
+  get '/visualisations/:visid' => 'visualisations#show'
+
+  get '/visualisations/all' => 'visualisations#all' 
+
+  delete '/visualisations/:visid' => 'visualisations#delete'
 end
