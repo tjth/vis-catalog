62d3b8472d8591b786db82fa53b42aa39a85ca1e
Bryan Liu <bryan.liu12@imperial.ac.uk>
2014-11-09 12:05:45 +0000

    Migration to add date field for timeslot

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104170642) do
+ActiveRecord::Schema.define(version: 20141109120013) do
 
   create_table "programmes", force: true do |t|
     t.integer  "vis_ID"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20141104170642) do
     t.time     "end_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "date"
   end
 
   create_table "users", force: true do |t|
