639cb3e18a11943a5454b2cf5f5b82619b56b5cb
Bryan Liu <bryan.liu12@imperial.ac.uk>
2014-11-12 12:27:33 +0000

    added appropriate belongs_to relations

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -1,3 +1,4 @@
 class Programme < ActiveRecord::Base
   belongs_to :visualisation
+  belongs_to :timeslot
 end
--- app/models/visualisation.rb
+++ app/models/visualisation.rb
@@ -1,5 +1,6 @@
 class Visualisation < ActiveRecord::Base
   enum content_type: [ :file, :weblink ]
+  belongs_to :user
   has_many :programmes
   has_many :playout_sessions
   attr_default :approved, false
