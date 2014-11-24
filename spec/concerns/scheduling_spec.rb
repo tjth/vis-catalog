acd889bb6391175c21bbee22983c4a0365eefa25
Bryan Liu <bryan.liu12@imperial.ac.uk>
2014-11-24 10:26:48 +0000

    Test for scheduling algorithm

--- spec/concerns/scheduling_spec.rb
+++ spec/concerns/scheduling_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Scheduling, :type => :concern do
     end
 
     it 'should return a programme with lowest no. of screen(s)' do
-      expect(prog.screens).to eq(Const.MIN_SCREENS)
+      expect(prog.screens).to eq(Const.MIN_NO_SCREENS)
     end
   end
 
@@ -44,8 +44,8 @@ RSpec.describe Scheduling, :type => :concern do
     end
   end
 
-  start_time = DateTime.new(2014, 9, 1, 12, 0, 0).utc
-  end_time = DateTime.new(2014, 9, 1, 13, 0, 0).utc
+  start_time = DateTime.new(2014, 9, 2, 12, 0, 0).utc
+  end_time = DateTime.new(2014, 9, 2, 13, 0, 0).utc
 
   describe '.clean_old_sessions' do
     it 'should clean all the existing session within the timeslot' do
@@ -63,36 +63,32 @@ RSpec.describe Scheduling, :type => :concern do
   end
 
   describe '.generate_schedule' do
-    context 'when total screen load equals to NO_OF_SCREENS' do
-      context 'and there is 1 programme only (overriding case)' do
+    context 'schedule playout with time proportional to priority' do
+      def getTotalPlaytime (visids, playouts)
+        remaining_playouts = playouts
+        playtimes = {}
+        visids.each do |visid|
+          associated_playouts = 
+            remaining_playouts.take_while{|playout| playout.visualisation_id == visid}
+          remaining_playouts = 
+            remaining_playouts.drop_while{|playout| playout.visualisation_id == visid}
+          
+          totalPlaytime = 0
+          associated_playouts.each do |playout|
+            totalPlaytime += (playout.end_time - playout.start_time)
+          end
 
-        it 'should do something' do
-          vis1 = Visualisation.create({:name => "Milan"})
-          vis2 = Visualisation.create({:name => "Green", :min_playtime => 2})
-          vis3 = Visualisation.create({:name => "Pink", :min_playtime => 3})
-
-          prog1 = Programme.create({:screens => 2, :priority => 3})
-          prog1.visualisation = vis1
-          prog2 = Programme.create({:screens => 1, :priority => 6})
-          prog2.visualisation = vis2
-          prog3 = Programme.create({:screens => 1, :priority => 9})
-          prog3.visualisation = vis3
-
-          expect(1).to be eq(1)
+          playtimes[visid] = totalPlaytime
         end
-
-      context 'and there is 2-4 programs (cycle-around case)' do
-          pending ": to finish writing the test"
+        return playtimes
       end
 
-    end
-  end
-
-  describe '.initQueue' do
-    context 'to fill in' do
-      it 'does something' do
+      it 'for one programme (overriding case)' do
+        pending 'write test'
+      end
+      it 'for more than one programme: example 1' do
         vis1 = Visualisation.create({:name => "Milan"})
-        vis2 = Visualisation.create({:name => "Green", :min_playtime => 2})
+        vis2 = Visualisation.create({:name => "Green", :min_playtime => 120})
         vis3 = Visualisation.create({:name => "Pink"})
 
         prog1 = Programme.create({:screens => 2, :priority => 1})
@@ -102,12 +98,43 @@ RSpec.describe Scheduling, :type => :concern do
         prog3 = Programme.create({:screens => 1, :priority => 4})
         prog3.visualisation = vis3
 
-        queue = initQueue([prog1, prog2, prog3])
-        expect(queue.min.first.prog).to be prog3
-        expect(queue.delete_min.first.prog).to be prog3
-        expect(queue.delete_min.first.prog).to be prog2
-        expect(queue.delete_min.first.prog).to be prog1
+        timeslot = Timeslot.create({:start_time => start_time,
+                                    :end_time => end_time})
+        timeslot.programmes << [prog1, prog2, prog3]
+        generate_schedule(timeslot)
+
+        playouts = PlayoutSession.where(start_time: start_time...end_time)
+
+        playtimes = getTotalPlaytime([vis1.id, vis2.id, vis3.id], playouts)
+        expect(playtimes).to be 1
       end
+    end  
+    
+    context 'should work for 2x2 screen configuration:' do
+      it 'no 1x2 vis should be on screens in two seperate rows' do
+        pending 'write test'
+      end
+    end 
+  end
+
+  describe '.initQueue' do
+    it 'initialise queue with increasing time for next playout' do
+      vis1 = Visualisation.create({:name => "Milan"})
+      vis2 = Visualisation.create({:name => "Green", :min_playtime => 120})
+      vis3 = Visualisation.create({:name => "Pink"})
+
+      prog1 = Programme.create({:screens => 2, :priority => 1})
+      prog1.visualisation = vis1
+      prog2 = Programme.create({:screens => 1, :priority => 5})
+      prog2.visualisation = vis2
+      prog3 = Programme.create({:screens => 1, :priority => 4})
+      prog3.visualisation = vis3
+
+      queue = initQueue([prog1, prog2, prog3])
+      expect(queue.min.first.prog).to be prog3
+      expect(queue.delete_min.first.prog).to be prog3
+      expect(queue.delete_min.first.prog).to be prog2
+      expect(queue.delete_min.first.prog).to be prog1
     end
   end
 
