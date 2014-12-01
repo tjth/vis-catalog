84698dc55a1d88a258ba4da941f296b1ded3c3ef
Emma <ekh12@ic.ac.uk>
2014-12-01 13:19:58 +0000

    ng show works, submit redirects to main, etc

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -2,7 +2,7 @@ app.controller('submitAdvertController', function($scope, $rootScope, $route) {
     $rootScope.page = {title: "Submit Advert", headerClass:"submit-advert", searchEnabled : false, class:"submit"}
 });
 
-app.controller('submitVisualisationController', function($scope, $rootScope, $route) {
+app.controller('submitVisualisationController', function($scope, $rootScope, $route, $location) {
     $rootScope.page = {title: "Submit Visualisation",  headerClass:"submit-visualisation", searchEnabled : false, class:"submit"}
     $scope.submit = { img:"assets/camera.png" }
     $scope.submitVisualistion = function() {
@@ -12,19 +12,33 @@ app.controller('submitVisualisationController', function($scope, $rootScope, $ro
 	fd.append('visualisation[vis_type]', 'vis');
   fd.append('visualisation[content_type]', value);
 	if (value == 'weblink'){
-      console.log("HELLO");
 	    fd.append('visualisation[content]', '');
 	} else {
 	    fd.append('visualisation[link]', '');
 	}
-	$.ajax({
-	    url: "/visualisations",
-	    data: fd,
-	    cache: false,
-	    contentType: false,
-	    processData: false,
-	    type: 'POST'
-	});
+  if (document.getElementById('screenshot').value == ''){
+      alert("Please submit a screenshot");
+  } else if (document.getElementById('name').value == ''){
+      alert("Please submit a name");
+  } else if (document.getElementById('url').value == '' && value == 'weblink'){
+      alert("Please submit a url");
+  } else if (document.getElementById('content').value == '' && value == 'file'){
+      alert("Please submit a file");
+  }
+  else {
+
+      $.ajax({
+	        url: "/visualisations",
+    	    data: fd,
+    	    cache: false,
+	        contentType: false,
+    	    processData: false,
+    	    type: 'POST'
+    	});
+      $location.path('/');
+      $route.reload();
+  }
+
       
     }
 });
