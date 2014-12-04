7dd4c16c592637028510c4ff694a1bc4179db532
Emma <ekh12@ic.ac.uk>
2014-12-04 09:01:29 +0000

    Revert "added advert pge"
    This reverts commit 087b36913bb38a39d0c020c898ba8e3a8b4dbf26.

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,17 +1,8 @@
 app.controller('submitAdvertController', function($scope, $rootScope, $route) {
-
-    if ($rootScope.user == null || $rootScope.user == undefined) {
-        showToast("Please log in to submit content");
-        $location.path("sign-in"); $location.search("return", "/submit/visualisation");  return;
-    }
-
     $rootScope.page = {title: "Submit Advert", headerClass:"submit-advert", searchEnabled : false, class:"submit"}
-    $scope.submitAdvert = function() {
-        submit('advert');
-    }
 });
 
-app.controller('submitVisualisationController', function($scope, $rootScope, $route) {
+app.controller('submitVisualisationController', function($scope, $rootScope, $route, $location) {
     if ($rootScope.user == null || $rootScope.user == undefined) {
         showToast("Please log in to submit content");
         $location.path("sign-in"); $location.search("return", "/submit/visualisation");  return;
@@ -22,47 +13,41 @@ app.controller('submitVisualisationController', function($scope, $rootScope, $ro
     $scope.options = ['weblink', 'file'];
     
     $scope.submitVisualistion = function() {
-        submit('visualistion');
+        var fd = new FormData(document.getElementById("submit-visualisation"));
+        var content_type = document.getElementById("content_type");
+        var value = content_type.options[content_type.selectedIndex].value;
+        fd.append('visualisation[vis_type]', 'vis');
+        fd.append('visualisation[content_type]', value);
+        console.log("key is " + localStorage.getItem("authentication_key"));
+        fd.append('visualisation[authentication_key]', localStorage.getItem("authentication_key"));
+        if (value == 'weblink'){
+            fd.append('visualisation[content]', '');
+        } else {
+            fd.append('visualisation[link]', '');
+        }
+        if (document.getElementById('screenshot').value == ''){
+          showToast("Please submit a screenshot");
+        } else if (document.getElementById('name').value == ''){
+          showToast("Please submit a name");
+        } else if (document.getElementById('url').value == '' && value == 'weblink'){
+          showToast("Please submit a url");
+        } else if (document.getElementById('content').value == '' && value == 'file'){
+          alert("Please submit a file");
+        } else {
+          $.ajax({
+                url: "/visualisations",
+                data: fd,
+                cache: false,
+                contentType: false,
+                processData: false,
+                type: 'POST'
+            });
+            showToast("Thanks for submitting! A moderator should approve your content shortly");
+          $location.path('/');
+          $route.reload();
+        }
+
+      
     }
 });
 
-function submit(type) {
-
-    var fd = new FormData(document.getElementById("submit-visualisation"));
-    if (type == 'visualisation') {
-        fd.append('visualisation[vis_type]', 'vis');
-        fd.append('visualisation[content_type]', value);
-        var content_type = document.getElementById("content_type");
-        var value = content_type.options[content_type.selectedIndex].value;
-    } else {
-        fd.append('visualisation[vis_type]', 'advert');
-    {
-    console.log("key is " + localStorage.getItem("authentication_key"));
-    fd.append('visualisation[authentication_key]', localStorage.getItem("authentication_key"));
-    if (value == 'weblink'){
-        fd.append('visualisation[content]', '');
-    } else {
-        fd.append('visualisation[link]', '');
-    }
-    if (document.getElementById('screenshot').value == '' && type == 'visualisation'){
-        showToast("Please submit a screenshot");
-    } else if (document.getElementById('name').value == ''){
-        showToast("Please submit a name");
-    } else if (document.getElementById('url').value == '' && value == 'weblink' && type == 'visualisation'){
-        showToast("Please submit a url");
-    } else if (document.getElementById('content').value == '' && value == 'file'){
-        showToast("Please submit a file");
-    } else {
-        $.ajax({
-            url: "/visualisations",
-            data: fd,
-            cache: false,
-            contentType: false,
-            processData: false,
-            type: 'POST'
-        });
-        showToast("Thanks for submitting! A moderator should approve your content shortly");
-        $location.path('/');
-        $route.reload();
-    }
-}
