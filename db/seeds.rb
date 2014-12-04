efbc04aa93ca57f102fa544815269fa2aabd5835
Andrew Higginson <VM>
2014-12-04 15:02:58 +0000

    polishing of moderation undo cloaking of timeslots test views

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.find_by_username("tjt12")
+user = User.find_by_username("ath12")
 if user != nil
   user.isAdmin = true
   user.save!
