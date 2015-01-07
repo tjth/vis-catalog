8e354e6fcd77ca197b82a21c5d87d1027281ae0d
Thomas Taylor-Hall <tjt12@ic.ac.uk>
2015-01-07 21:27:31 +0000

    Removed testuser from db

--- app/models/testuser.rb
+++
@@ -1,7 +0,0 @@
-class Testuser < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
-         :kerberos_authenticatable
-end
